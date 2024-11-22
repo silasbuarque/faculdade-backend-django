5deae495ea680539eae01052410a03a234ada63d
sillasphablo <sillinhas_phablo@hotmail.com>
2024-11-22 18:20:23 -0300

    Incluindo a Biblioteca de CORs para evitar conflito quando houver execução na mesma máquina

--- faculdadeProjetoBackend/settings.py
+++ faculdadeProjetoBackend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'faculdadeProjetoBackend.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
